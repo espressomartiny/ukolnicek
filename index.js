8949b5607761fe2f086afe37f475fa2ee159f62f
Martina Vokáčová <martinavokacova@gmail.com>
2022-10-29 23:41:28 +0200

    Task

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('funguju!');
+console.log("funguju!");
+
+fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
+
+fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks?done=true")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
+
+  
+const Task = (props) => {
+  const { id, name, due, done } = props;
+
+  let checkClass = "";
+  if (done) {
+    checkClass = "✓";
+  }
+
+  return;
+  `
+      <div class="task">
+      <div class="task__body">
+        <div class="task__name">${name}</div>
+        <div class="task__due">${due}</div>
+      </div>
+      <div class="task__done">${checkClass}</div>
+    </div>
+    `;
+};
