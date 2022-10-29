0f083e33e952eb722154eecc29ababb67a93a2ca
Martina Vokáčová <martinavokacova@gmail.com>
2022-10-29 23:48:06 +0200

    render

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 console.log("funguju!");
 
-fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+// fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
 
-fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks?done=true")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+// fetch("https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks?done=true")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
 
-  
 const Task = (props) => {
-  const { id, name, due, done } = props;
+  const { name, due, done } = props;
 
   let checkClass = "";
   if (done) {
     checkClass = "✓";
   }
 
-  return;
-  `
-      <div class="task">
+  return `
+ <div class="task">
       <div class="task__body">
         <div class="task__name">${name}</div>
         <div class="task__due">${due}</div>
@@ -31,4 +29,9 @@ const Task = (props) => {
       <div class="task__done">${checkClass}</div>
     </div>
     `;
+};
+
+const renderTasks = (items) => {
+  const tasksElement = document.querySelector(".todo__tasks");
+  tasksElement.innerHTML = items.map((item) => Task(item)).join("");
 };
